0c0887821bd16aa0b4fe76048018244f226d3266
Raktim <raktim@example.com>
2026-07-16 19:44:35 +0530

    Javascript Assignment

---
+++ Day 9/index.js
@@ -0,0 +1,17 @@
+let num = 10;
+
+for (let i = 0; i < num; i = i + 1) {
+    console.log(i);
+}
+
+let std = ["Nishita", "Rezwan", "Rabi", "Aryan", "Mriganka", "Supriya", "Mahavir", "Nantelo", "Bhaskar"];
+
+for (let i = 0; i < std.length; i++) {
+    console.log(std[i]);
+}
+
+let firststudent = std[0];
+
+for (let i = 0; i < firststudent.length; i++) {
+    console.log(firststudent[i]);
+}
